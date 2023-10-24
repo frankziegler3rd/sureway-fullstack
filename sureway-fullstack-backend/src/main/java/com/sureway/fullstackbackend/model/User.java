df90b4bdeb3cd7a11f4474efc0b8494329eab8bf
Frank Ziegler <38428365+frankziegler3rd@users.noreply.github.com>
2023-10-24 14:50:03 -0400

    Added backend Spring Boot application

---
+++ sureway-fullstack-backend/src/main/java/com/sureway/fullstackbackend/model/User.java
@@ -0,0 +1,49 @@
+package com.sureway.fullstackbackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer account_id;
+
+    private String uname;
+    private String password;
+    private String email;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
